91e4fdb3717e37b9263a5c6b7ea5830da88af60f
darshfi <darshfi6911@gmail.com>
2024-10-03 20:03:05 +0530

    ok

---
+++ app/src/main/java/com/example/envii/di/CameraRepositoryModule.kt
@@ -0,0 +1,20 @@
+package com.example.envii.di
+
+import com.example.envii.data.repository.CameraRepositoryImpl
+import com.example.envii.domain.repository.CameraRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class CameraRepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindCameraRepository(
+        cameraRepositoryImpl: CameraRepositoryImpl
+    ): CameraRepository
+}
